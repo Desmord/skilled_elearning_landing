90a6cd55047bdab81ad169c5c07dc5f085839d38
Mikołaj Chojnacki <mikolaj.chojnacki@wp.pl>
2023-05-09 17:39:01 +0200

    Init project.

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './global.scss'
+
+function App() {
+  return (
+    <div >
+    asfsdafasd Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, repellendus.
+    </div>
+  );
+}
+
+export default App;
